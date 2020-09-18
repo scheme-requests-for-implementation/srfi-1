f8b37cbe681936bc4f2aac89ffb798757047c038
Arthur A. Gleckler <srfi@speechcode.com>
2020-09-18 11:08:42 -0700

    Fix bug reported by Marc Nieper-Wißkirchen.

--- srfi-1-reference.scm
+++ srfi-1-reference.scm
@@ -1238,7 +1238,7 @@
 		 (new-tail (recur (delete x tail elt=))))
 	    (if (eq? tail new-tail) lis (cons x new-tail)))))))
 
-(define (delete-duplicates! lis maybe-=)
+(define (delete-duplicates! lis . maybe-=)
   (let ((elt= (:optional maybe-= equal?)))
     (check-arg procedure? elt= delete-duplicates!)
     (let recur ((lis lis))
